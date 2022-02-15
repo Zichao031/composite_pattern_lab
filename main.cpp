056437636c6b1a0d52b5b8a7da41309a18139adc
WwwwwFeng <97133827+WwwwwFeng@users.noreply.github.com>
2022-02-15 12:52:11 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 #include "base.hpp"
+#include "base.hpp"
+#include "op.hpp"
+#include "rand.hpp"
+#include "add.hpp"
+#include "sub.hpp"
+#include "mult.hpp"
+#include "div.hpp"
+#include "pow.hpp"
 
 int main() {
     // This is a very basic main, and being able to correctly execute this main
