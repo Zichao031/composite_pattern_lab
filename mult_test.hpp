8686a15cb58eac373e35f4d373bdb88e4df1555c
WwwwwFeng <97133827+WwwwwFeng@users.noreply.github.com>
2022-02-15 13:30:53 -0800

    Update mult_test.hpp

--- mult_test.hpp
+++ mult_test.hpp
@@ -9,42 +9,42 @@
 TEST(MultTest, MultEvaluatePostive){
     Op* value1 = new Op(2.0);
     Op* value2 = new Op(3.0);
-    Op* value3 = new Mult(value1, value2);
+    Mult* value3 = new Mult(value1, value2);
     EXPECT_EQ(6.0, value3->evaluate());
 }
 
 TEST(MultTest, MultStringifyPostive){
     Op* value1 = new Op(2.0);
     Op* value2 = new Op(3.0);
-    Op* value3 = new Mult(value1, value2);
+    Mult* value3 = new Mult(value1, value2);
     EXPECT_EQ("(2.000000 * 3.000000)", value3->Stringify());
 }
 
 TEST(MultTest, MultEvaluateNegative){
     Op* value1 = new Op(-2.0);
     Op* value2 = new Op(3.0);
-    Op* value3 = new Mult(value1, value2);
+    Mult* value3 = new Mult(value1, value2);
     EXPECT_EQ(-6.0, value3->evaluate());
 }
 
 TEST(MultTest, MultStringifyNegative){
     Op* value1 = new Op(-2.0);
     Op* value2 = new Op(3.0);
-    Op* value3 = new Mult(value1, value2);
+    Mult* value3 = new Mult(value1, value2);
     EXPECT_EQ("(-2.000000 * 3.000000)", value3->Stringify());
 }
 
 TEST(MultTest, MultEvaluateZero){
     Op* value1 = new Op(0.0);
     Op* value2 = new Op(3.0);
-    Op* value3 = new Mult(value1, value2);
+    Mult* value3 = new Mult(value1, value2);
     EXPECT_EQ(0.0, value3->evaluate());
 }
 
 TEST(MultTest, MultStringifyZero){
     Op* value1 = new Op(0.0);
     Op* value2 = new Op(3.0);
-    Op* value3 = new Mult(value1, value2);
+    Mult* value3 = new Mult(value1, value2);
     EXPECT_EQ("(0.000000 * 3.000000)", value3->Stringify());
 }
 
