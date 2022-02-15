b9754aa3cb57483b33c29d1eae18748feebd33df
WwwwwFeng <97133827+WwwwwFeng@users.noreply.github.com>
2022-02-15 13:13:34 -0800

    Create add.hpp

---
+++ add.hpp
@@ -0,0 +1,25 @@
+#ifndef __ADD_HPP__
+#define __ADD_HPP__
+
+#include "base.hpp"
+
+class Add : public Base {
+    private:
+        Base* left;
+        Base* right;
+        
+    public:
+        Add(Base* lhs, Base* rhs) : Base() {
+            left = lhs;
+            right = rhs;
+        }
+        double evaluate() { 
+            return left->evaluate() + right->evaluate();
+        }
+        std::string stringify() { 
+            return "(" + left->stringify() + "+" + right->stringify() + ")";
+        }
+
+};
+
+#endif //__ADD_HPP__
