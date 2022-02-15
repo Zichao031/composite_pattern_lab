9d545fe1a61d19f5f8711f32866be02c8b7b6435
WwwwwFeng <97133827+WwwwwFeng@users.noreply.github.com>
2022-02-15 13:35:27 -0800

    Create div.hpp

---
+++ div.hpp
@@ -0,0 +1,27 @@
+
+#ifndef __DIV_HPP__
+#define __DIV_HPP__
+
+#include "base.hpp"
+
+class Div : public Base {
+    public:
+        Div(Base* lhs, Base* rhs) : Base() {
+            left = lhs;
+            right = rhs;
+        }
+
+        double evaluate() {
+            return left->evaluate() / right->evaluate();
+        }
+
+        std::string stringify() {
+            return '(' + left->stringify() + " / " + right->stringify() + ')';
+        }
+        
+    private:
+        Base* left;
+        Base* right;
+};
+
+#endif
