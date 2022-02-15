2ee108d714daae6db17cc9d8b0b1e97eabd0b801
WwwwwFeng <97133827+WwwwwFeng@users.noreply.github.com>
2022-02-15 13:40:14 -0800

    Create div_test.hpp

---
+++ div_test.hpp
@@ -0,0 +1,51 @@
+#ifndef __DIV_TEST_HPP__
+#define __DIV_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "op.hpp"
+#include "div.hpp"
+
+TEST(DivTest, DivEvaluatePostive){
+    Op* value1 = new Op(6.0);
+    Op* value2 = new Op(3.0);
+    Div* value3 = new Div(value1, value2);
+    EXPECT_EQ(2.0, value3->evaluate());
+}
+
+TEST(DivTest, DivStringifyPostive){
+    Op* value1 = new Op(6.0);
+    Op* value2 = new Op(3.0);
+    Div* value3 = new Div(value1, value2);
+    EXPECT_EQ("(6.000000 / 3.000000)", value3->stringify());
+}
+
+TEST(DivTest, DivEvaluateNegative){
+    Op* value1 = new Op(-6.0);
+    Op* value2 = new Op(3.0);
+    Div* value3 = new Div(value1, value2);
+    EXPECT_EQ(-2.0, value3->evaluate());
+}
+
+TEST(DivTest, DivStringifyNegative){
+    Op* value1 = new Op(-6.0);
+    Op* value2 = new Op(3.0);
+    Div* value3 = new Div(value1, value2);
+    EXPECT_EQ("(-6.000000 / 3.000000)", value3->stringify());
+}
+
+TEST(DivTest, DivEvaluateZero){
+    Op* value1 = new Op(0.0);
+    Op* value2 = new Op(3.0);
+    Div* value3 = new Div(value1, value2);
+    EXPECT_EQ(0.0, value3->evaluate());
+}
+
+TEST(DivTest, DivStringifyZero){
+    Op* value1 = new Op(0.0);
+    Op* value2 = new Op(3.0);
+    Div* value3 = new Div(value1, value2);
+    EXPECT_EQ("(0.000000 / 3.000000)", value3->stringify());
+}
+
+#endif
