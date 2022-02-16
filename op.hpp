56d63f92cf065d0b6b935ea5d54bab83366c9a24
Zichao031 <zxiao031@ucr.edu>
2022-02-16 14:09:38 -0800

    Add op.hpp and op_test.hpp

--- op.hpp
+++ op.hpp
@@ -2,13 +2,17 @@
 #define __OP_HPP__
 
 #include "base.hpp"
+#include <string>
 
 class Op : public Base {
+    private:
+        double val = 0;
     public:
-        Op(double value) : Base() { }
-    
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value) : Base() {
+            val = value;
+        }  //?
+        virtual double evaluate() { return val; }
+        virtual std::string stringify() { return std::to_string(val); }
 };
 
 #endif //__OP_HPP__
