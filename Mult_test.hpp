34b18c97308cc4712650b314172a6a3c486ba86e
Zichao031 <zxiao031@ucr.edu>
2022-02-16 16:14:27 -0800

    Update Mult_test.hpp

--- Mult_test.hpp
+++ Mult_test.hpp
@@ -34,6 +34,20 @@ TEST(MultTest, MultStringifyNegative){
     EXPECT_EQ("(-2.000000 * 3.000000)", value3->stringify());
 }
 
+TEST(MultTest, MultEvaluateNegative2){
+    Op* value1 = new Op(-5.0);
+    Op* value2 = new Op(-3.0);
+    Mult* value3 = new Mult(value1, value2);
+    EXPECT_EQ(value3->evaluate(), 15);
+}
+
+TEST(MultTest, MultStringifyNegative2){
+    Op* value1 = new Op(-2.0);
+    Op* value2 = new Op(-3.0);
+    Mult* value3 = new Mult(value1, value2);
+    EXPECT_EQ("(-2.000000 * -3.000000)", value3->stringify());
+}
+
 TEST(MultTest, MultEvaluateZero){
     Op* value1 = new Op(0.0);
     Op* value2 = new Op(3.0);
@@ -48,4 +62,17 @@ TEST(MultTest, MultStringifyZero){
     EXPECT_EQ("(0.000000 * 3.000000)", value3->stringify());
 }
 
+TEST(MultTest, MultEvaluateZero2){
+    Op* value1 = new Op(0.0);
+    Op* value2 = new Op(0.0);
+    Mult* value3 = new Mult(value1, value2);
+    EXPECT_EQ(0.0, value3->evaluate());
+}
+
+TEST(MultTest, MultStringifyZero2){
+    Op* value1 = new Op(0.0);
+    Op* value2 = new Op(0.0);
+    Mult* value3 = new Mult(value1, value2);
+    EXPECT_EQ("(0.000000 * 0.000000)", value3->stringify());
+}
 #endif
