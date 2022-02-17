32d494ba298fe9a7444912664cb0417c28b5cf6c
Zichao031 <zxiao031@ucr.edu>
2022-02-16 16:08:16 -0800

    Add Sub_test.hpp

--- Sub_test.hpp
+++ Sub_test.hpp
@@ -22,16 +22,16 @@ TEST(SubTest, SubStringifyPostive) {
 
 TEST(SubTest, SubEvaluateNegative) {
     Op* value1 = new Op(-1.0);
-    Op* value2 = new Op(-2.0);
+    Op* value2 = new Op(-5.0);
     Sub* test = new Sub(value1, value2);
-    EXPECT_EQ(test->evaluate(), 1.0);
+    EXPECT_EQ(test->evaluate(), 4.0);
 }
 
 TEST(SubTest, SubStringNegative) {
     Op* value1 = new Op(-1.0);
-    Op* value2 = new Op(-2.0);
+    Op* value2 = new Op(-5.0);
     Sub* test = new Sub(value1, value2);
-    EXPECT_EQ("(-1.000000 - -2.000000)", test->stringify());
+    EXPECT_EQ("(-1.000000 - -5.000000)", test->stringify());
 }
 
 TEST(SubTest, SubEvaluateZero) {
@@ -46,6 +46,20 @@ TEST(SubTest, SubStringZero) {
     Op* value2 = new Op(2.0);
     Sub* test = new Sub(value1, value2);
     EXPECT_EQ("(0.000000 - 2.000000)", test->stringify());
+}
+
+TEST(SubTest, SubEvaluateZero2) {
+    Op* value1 = new Op(0.0);
+    Op* value2 = new Op(0.0);
+    Sub* test = new Sub(value1, value2);
+    EXPECT_EQ(0.0, test->evaluate());
+}
+
+TEST(SubTest, SubStringZero2) {
+    Op* value1 = new Op(0.0);
+    Op* value2 = new Op(0.0);
+    Sub* test = new Sub(value1, value2);
+    EXPECT_EQ("(0.000000 - 0.000000)", test->stringify());
 }
 
 #endif //__SUB_TEST_HPP__
