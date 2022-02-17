b3d0af2c87e690b1278be5fec657d0b47b9d4f4f
Zichao031 <zxiao031@ucr.edu>
2022-02-17 10:54:50 -0800

    Modify Pow.hpp

--- Pow.hpp
+++ Pow.hpp
@@ -11,11 +11,11 @@ public:
         right = rhs;
     }
 
-    double evaluate() {
+    virtual double evaluate() {
         return pow(left->evaluate(), right->evaluate());
     }
 
-    std::string stringify() {
+    virtual std::string stringify() {
         return '(' + left->stringify() + " ** " + right->stringify() + ')';
     }
 
